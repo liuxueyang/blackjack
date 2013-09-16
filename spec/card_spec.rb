5c49e047ad55a3a0b26ecae5632510a008d23cb9
patmood <patrick.n.moody@gmail.com>
2013-09-16 13:17:55 -0700

    card specs

--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -3,16 +3,18 @@ require 'spec_helper'
 describe Card do
   subject { Card.new('Hearts','3',3) }
 
-  it 'has readable suit' do
-    subject.suit.should == 'Hearts'
-  end
+  context 'readable attributes' do
+    it 'has readable suit' do
+      subject.suit.should == 'Hearts'
+    end
 
-  it 'has readable face' do
-    subject.face.should == '3'
-  end
+    it 'has readable face' do
+      subject.face.should == '3'
+    end
 
-  it 'has readable value' do
-    subject.value.should == 3
+    it 'has readable value' do
+      subject.value.should == 3
+    end
   end
 
   describe '#is_ace?' do
@@ -28,7 +30,7 @@ describe Card do
 
   describe '#to_s' do
     it 'returns a readable description of card' do
-      pending
+      subject.to_s.should == "3 of Hearts"
     end
   end
 end
