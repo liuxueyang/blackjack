08cff1ed2d469d316045da5d20639e3293cb1254
patmood <patrick.n.moody@gmail.com>
2013-09-16 01:56:38 -0700

    use attr_reader calls instead of instance variables

--- player.rb
+++ player.rb
@@ -11,12 +11,13 @@ class Player
   end
 
   def double_bet
-    @stack -= @bet
-    @bet = @bet*2
+    stack -= bet
+    bet = bet*2
   end
 
   def hand_total
-    total = @hand.map(&:value).reduce(:+)
+    raise "No cards in hand" if hand.length == 0
+    total = hand.map(&:value).reduce(:+)
     if has_ace? && total <= 11
       total += 10
     end
@@ -24,7 +25,7 @@ class Player
   end
 
   def display_total
-    total = @hand.map(&:value).reduce(:+)
+    total = hand.map(&:value).reduce(:+)
     if has_ace? && total <= 11
       "#{total} or #{total+10}"
     else
@@ -33,21 +34,21 @@ class Player
   end
 
   def summary
-    @hand.reduce("====== #{@name} ======\n"){ |memo,card| memo + "#{card}\n" } +
+    hand.reduce("====== #{name} ======\n"){ |memo,card| memo + "#{card}\n" } +
     "----------\nTotal: #{display_total}\n"
   end
 
   def has_ace?
-    @hand.each { |card| return true if card.is_ace? }
+    hand.each { |card| return true if card.is_ace? }
     return false
   end
 
   def status
-    if hand_total == 21 && @hand.length == 2
+    if hand_total == 21 && hand.length == 2
       :blackjack
     elsif hand_total > 21
       :bust
-    elsif hand_total == 21 || @stand == true
+    elsif hand_total == 21 || stand == true
       :stand
     elsif hand_total < 21
       :ready
@@ -58,9 +59,9 @@ class Player
 
   def play_options
     options = ["hit","stand"]
-    if @hand.length == 2 && @stack > @bet
+    if hand.length == 2 && stack > bet
       options << "double"
-      if @hand[0].face == @hand[1].face
+      if hand[0].face == hand[1].face
         options << "split"
       end
     end
