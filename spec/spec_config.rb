1d3847477cf8f57e38313a28ff4e657a7fcd2337
patmood <patrick.n.moody@gmail.com>
2013-09-16 16:42:50 -0700

    dedicated config for specs

---
+++ spec/spec_config.rb
@@ -0,0 +1,6 @@
+STARTING_STACK = 1000
+NUMBER_OF_DECKS = 2
+STATUS_MESSAGE = {:blackjack => "Blackjack!",
+                  :ready => "Waiting for action...",
+                  :stand => "Stand",
+                  :bust => "Bust hand!"}
